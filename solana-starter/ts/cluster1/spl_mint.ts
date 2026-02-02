dfe1ad43fbc8c897b054ea24f0a0d21360cf12a8
Avinash-kumar-82 <avinashkumar829114@gmail.com>
2026-02-02 14:04:50 +0530

    spl_mint.ts updated

--- solana-starter/ts/cluster1/spl_mint.ts
+++ solana-starter/ts/cluster1/spl_mint.ts
@@ -1,9 +1,23 @@
-import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import {
+    Keypair,
+    Connection,
+    Commitment,
+    PublicKey,
+    Transaction,
+    sendAndConfirmTransaction,
+    SendTransactionError
+} from "@solana/web3.js";
+import {
+    getOrCreateAssociatedTokenAccount,
+    mintTo,
+    createMintToInstruction,
+} from '@solana/spl-token';
 
-// Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+import PAYER_ADDRESS from '../keypairs/payer_keypair_1.json';
+import MINT_ADDRESS from '../keypairs/mint_keypair_1.json';
+
+const payer = Keypair.fromSecretKey(new Uint8Array(PAYER_ADDRESS));
+const mint = Keypair.fromSecretKey(new Uint8Array(MINT_ADDRESS));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
@@ -11,19 +25,52 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 const token_decimals = 1_000_000n;
 
-// Mint address
-const mint = new PublicKey("<mint address>");
-
 (async () => {
     try {
         // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        const ata = await getOrCreateAssociatedTokenAccount(
+            connection,
+            payer,
+            mint.publicKey,
+            payer.publicKey
+        );
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+
+        // "Mint to ATA" Instruction
+        const mintIx = createMintToInstruction(
+            mint.publicKey,
+            ata.address,
+            payer.publicKey,
+            token_decimals
+        );
+
+        const mintTx = new Transaction().add(
+            mintIx
+        );
+
+        console.log(`Your mint txid: ${mintTx}`);
+
+        const signature = await sendAndConfirmTransaction(
+            connection,
+            mintTx,
+            [payer]
+        );
+
+        console.log(
+            `
+            Token minted to ATA !!
+            Check transaction:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet
+            `
+        );
+
+    } catch (error) {
+        if (error instanceof SendTransactionError) {
+            const transactionErrorDetails = await error.getLogs(connection);
+            console.log(transactionErrorDetails);
+            return
+        }
+
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
